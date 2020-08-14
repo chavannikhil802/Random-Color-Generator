d1fcc678ce8adda456cca4607c55d4bf362dd30e
Nikhil Narendra Chavan <chavannikhil802@gmail.com>
2020-08-14 23:23:15 +0530

    Adding comments wherever required

--- Random-Color-Generator/js/script.js
+++ Random-Color-Generator/js/script.js
@@ -1,23 +1,22 @@
+// FUNCTION THAT GENERATES A RANDOM NUMBER COLOR IN RGB FORMAT
 function randomColor() {
+    // GENERATE A RANDOM NUMBER UPTO 255
     return Math.floor(Math.random() * 255) + 1;
 }
 
-function boxShadow(colorValue) {
-    return "0 2px 2px 0"+colorValue+", 5px 6px 40px 5px"+colorValue;
-}
-
 function generateRandomColor() {
+    // R,G, B VARIABLES TO STORE THE RANDOM GENERATED VALUES
     var r = randomColor();
     var g = randomColor();
     var b = randomColor();
 
+    // APPENDING THE RGB VALUES IN ONE VARIABLE
     var background = "rgb("+r+", "+g+", "+b+")";
-
-    console.log(background);
 
     var panel = document.getElementById("row-color-panel");
     var button = document.getElementById("button");
 
+    // APPLYING THE RANDOM COLOR TO THE COLOR PANEL AND BUTTON
     panel.style.backgroundColor = background;
     button.style.color = background;
     button.style.border = background;
